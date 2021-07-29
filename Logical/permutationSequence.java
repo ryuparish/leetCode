3b99022135f5c7bd255c7b6163879655c101f55f
ryuparish <ryuparish1115@gmail.com>
2021-07-29 17:03:00 -0500

    Added permutationSequence in java

--- Logical/permutationSequence.java
+++ Logical/permutationSequence.java
@@ -1,5 +1,29 @@
 class Solution {
     public String getPermutation(int n, int k) {
+        // Create the choices array
+        ArrayList<Integer> choices = new ArrayList<Integer>();
+        for(int i = 1; i <= n; ++i){
+            choices.add(i);
+        }
+
+        StringBuilder myAnswer = new StringBuilder();
+        // Create the factorial array
+        ArrayList<Integer> factorials = new ArrayList<Integer>();
+        factorials.add(1);
+        int currFactorial = 1;
+        for(int i = 1; i < n; ++i){
+            currFactorial *= i;
+            factorials.add(currFactorial);
+        }
         
+        // Build the string using the formulas
+        int currIdx = 0;
+        for(int i = 1; i <= n; ++i){
+            currIdx = k/factorials[n-i];
+            myAnswer.append(Integer.toString(choices[currIdx]));
+            choices.remove(currIdx);
+            k -= currIdx * factorialx[n-i];
+        }
+        return myAnswer;
     }
 }
