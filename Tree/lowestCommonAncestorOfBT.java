c34be7712a7de4807f8e8b4f2637b07f02ec679b
ryuparish <ryuparish1115@gmail.com>
2021-09-07 21:29:58 -0500

    Added lowestCommonAncestorBT.java

---
+++ Tree/lowestCommonAncestorOfBT.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// In order to find the solution to this problem, you need to have both children between the currNode
+// or you need to have one child below the node and the currnode itself has to be the other child.
+class Solution {
+    TreeNode answer;
+    // This algorithm will continue to run even after the answer is found
+    private boolean findAnswer(TreeNode root, int pval, int qval){
+        if(root == null){return false;}
+        int mid = (root.val == pval || root.val == qval) ? 1 : 0;
+        int left = findAnswer(root.left, pval, qval) ? 1 : 0;
+        int right = findAnswer(root.right, pval, qval) ? 1 : 0;
+        if(left+right+mid >= 2){this.answer = root;}
+        return (mid+right+left > 0);
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int mid=0, left=0, right=0;
+        findAnswer(root, p.val, q.val);
+        return this.answer;
+    }
+}
