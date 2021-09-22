b11ea57b0dfa0920138b0388c3f5e72b92baddaf
ryuparish <ryuparish1115@gmail.com>
2021-09-22 13:02:52 -0500

    Added some revisions

--- Logical/kthLargestElementinArray.java
+++ Logical/kthLargestElementinArray.java
@@ -1,11 +1,3 @@
-// TC: O(NlogN), SC: O(1)
-//class Solution {
-//    public int findKthLargest(int[] nums, int k) {
-//        Arrays.sort(nums);
-//        return nums[nums.length-k];
-//    }
-//}
-
 // QuickSelect
 // If you think about it the pivot of recursive call for quicksort is in the correct position
 // if the list were to be sorted entirely. All values left would be less than it and all values
@@ -13,51 +5,52 @@
 // k to be the pivot in this case.
 class Solution {
     private void swap(int[] nums, int a, int b){
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
+        int temp = nums[b];
+        nums[b] = nums[a];
+        nums[a] = temp;
+        return;
     }
     
     public int findKthLargest(int[] nums, int k) {
-        quickSelect(nums, 0, nums.length-1, nums.length - k);
-        for(int i = 0; i < nums.length; ++i){
-            System.out.print(nums[i]);
-        }
-        System.out.println("");
-        return nums[nums.length-k];
+		// The kth LARGEST, so length - k (k is guaranteed to be at least 1)
+        quickSelect(nums, 0, nums.length-1, nums.length-k);
+        return nums[nums.length - (k)];
     }
     
-    private void quickSelect(int[] nums, int start, int end, int k){
+    private void quickSelect(int[] nums, int start, int end, int target){
+        
+        // Conditional is needed for recurrence to work
+        // This must be < rather than <= because if they are equal 
         if(start < end){
-            int pindex = partition(nums, start, end);
-            if(pindex == k){return;}
-            if(pindex > k){
-                quickSelect(nums, start, pindex-1, k);
+            int partition_ = partition(nums, start, end);
+            if(partition_ == target){return;}
+			
+			// We want to move in the direction that contains the target index
+            else if(partition_ > target){
+                quickSelect(nums, start, partition_-1, target);
             }
-            else{quickSelect(nums, pindex+1, end, k);}
+            else{
+                quickSelect(nums, partition_+1, end, target);
+            }
         }
     }
     
     private int partition(int[] nums, int start, int end){
         int pindex = start + new Random().nextInt(end - start);
-        int pindexVal = nums[pindex];
-        int swapperIdx = start;
-        // Crucial step for a case like this:
-        //      6,7,8,4,1,2,3,2,1
-        //            ^
-        //          pindex(this index will be overwritten and the final swapperIdx will not
-        //          contain the pindexVal)
-        swap(nums, end, pindex);
+        int pVal = nums[pindex];
+        int swapper = start;
+
+		// Swap to safekeep pivot value
+        swap(nums, pindex, end);
         for(int i = start; i <= end; ++i){
-            // since this will activate and possibly swap start with start,
-            // it is guaranteed that the value pindex is on will be larger than pindexVal
-            // or on the same index as i.
-            if(nums[i] < pindexVal){
-                swap(nums, swapperIdx, i);
-                swapperIdx = swapperIdx+1;
+            if(nums[i] < pVal){
+                swap(nums, swapper, i);
+                swapper++;
             }
         }
-        swap(nums, swapperIdx, end);
-        return swapperIdx;
+
+		// Swap back to rightful sorted spot
+        swap(nums, end, swapper);
+        return swapper;
     }
 }
--- humblers/rotatedSortedArray.java
+++ humblers/rotatedSortedArray.java
@@ -1,57 +1,39 @@
-// Major ass kicking by the mathematical relationships of binary search along with the mathematical process of Binary Search itself.
-// What comes in handy in this solution is this:
-// 1. Modulo operator.
-// 2. Binary Search Twice (one to search for the smallest value and one to use for possibly finding the target)
+// Gist: There is a guarantee for the array to be rotated at one point. We need to run a binary
+// search that is slightly augmented on the conditionals. At any split, there will always be
+// at least one side that contains a monotonically increasing set of numbers, or what I call
+// the "certain" side. If the target is in that side then move to that side, if not move to 
+// the other and it will be the same problem but smaller, a sub-problem. Divide and Conquer.
 class Solution {
     public int search(int[] nums, int target) {
-        // First I need to search for k with Binary Search
-        // The while loop will either move up lower boundary to a point where midValue is less than upperBoundaryValue, or move back upperBoundary
-        // until either lowerBoundary is moved up again, or mid finds the rotation-1 position and pushes lowerBoundary onto the rotationIndex.
-        // The reason this is valid is because Proof by contradiction that mid < hi: if mid==hi, then lo==hi and loop would have been terminated.
-        int lowerBound = 0, upperBound = nums.length-1, midIndex = 0, shiftedMid = 0;
-        while(lowerBound < upperBound){
-            midIndex= (lowerBound + upperBound) /2;
-            // In the case we get lucky and find the index whilst searching for
-            // the lowest value
-            if(nums[midIndex] == target){
-                return midIndex;
-            }
-            else if(nums[midIndex] > nums[upperBound]){
-                lowerBound = midIndex + 1;
-            }
-            else{
-                upperBound = midIndex;
-            }
-        }
-        // The rotationIndex and k are not the same. The rotation index will tell us how much the list has been rotated.
-        int rotationIndex = lowerBound;
-        lowerBound = 0; upperBound = nums.length-1;
-        // BEWARE OF EDGE CASES EXPLAINED BELOW WITH EXPLANATION
-        // We can perform binary search regularly and just account for the rotation because although the lower and upper bound are not 
-        // accurate, the place in which the search happens is accurate and the placement of the new bounds only affects the spot searched, albeit correctly.
-        // If too high, the spot still moves down and vice versa, therefore the oscillation technique is STILL valid.
+        int lo = 0, hi = nums.length-1, mid, midVal;
         
-        // This is a good lesson on what binary search mathematically is performing.
-        
-        // RELATIONSHIP OF <=, < and -1,  +1.
-        // We MUST use +1 for avoiding the "move lowerbound by 1 and loop forever" problem, and then -1 for the upperBoundary along with <= in the
-        // case we have only a single number in the list (we must use <= because lowerBoundary == upperBoundary in the very beginning). 
-        // The -1 to upperBoundary is to prevent the same infinite loop due to the <=. 
-         
-        while(lowerBound <= upperBound){
-            midIndex = ((lowerBound + upperBound) /2);
-            shiftedMid = (midIndex + rotationIndex) %nums.length;
-            if(nums[shiftedMid] == target){
-                return shiftedMid;
-            }
-            else if(nums[shiftedMid] < target){
-                lowerBound = midIndex + 1;
-            }
-            // target is less than the lowerBound value but greater than the midIndex value
-            else{
-                upperBound = midIndex - 1;
+        // We can allow overlap, just not exceedence
+        while(lo <= hi){
+            mid = lo + (hi - lo)/2;
+            midVal = nums[mid];
+            if(nums[mid] == target){return mid;}
+            
+            // When the left side if "certain"
+            else if(nums[lo] <= midVal){
+
+                // And target is in the controlled side
+                if(target >= nums[lo] && midVal > target){
+                    hi = mid-1;
+                }
+                else{
+                    lo = mid+1;
+                }
             }
             
+            // When the right side is "certain"
+            else{
+                if(target <= nums[hi] && target > midVal){
+                    lo = mid+1;
+                }
+                else{
+                    hi = mid-1;
+                }
+            }
         }
         return -1;
     }
