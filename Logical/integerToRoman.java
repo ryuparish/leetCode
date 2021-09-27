3d0ea9e9d3d29ae5b31078d6edd01ec4ec35d416
ryuparish <ryuparish1115@gmail.com>
2021-09-26 17:27:31 -0700

    Added integerToRoman.java

--- Logical/integerToRoman.java
+++ Logical/integerToRoman.java
@@ -1,60 +1,69 @@
-import java.util.*;
-import java.lang.Math;
 class Solution{
     public static String intToRoman(int num){
-        HashMap<Double, Character> myMap = new HashMap<>();
-        myMap.put(1.0,'I');
-        myMap.put(5.0,'V');
-        myMap.put(10.0,'X');
-        myMap.put(50.0,'L');
-        myMap.put(100.0,'C');
-        myMap.put(500.0,'D');
-        myMap.put(1000.0,'M');
+        HashMap<Integer, Character> myMap = new HashMap<>();
+        myMap.put(1,'I');
+        myMap.put(5,'V');
+        myMap.put(10,'X');
+        myMap.put(50,'L');
+        myMap.put(100,'C');
+        myMap.put(500,'D');
+        myMap.put(1000,'M');
 
         int digits = 0;
-        double currentDigit;
+        int currentDigit;
         int origNum = num;
-        String romanNum = "";
+        StringBuilder romanNum = new StringBuilder();
+        
+        // Counting the number of digits in the number
         while(num > 0){
             num /= 10;
             ++digits;
         }
         for(int i = 0; i < digits; ++i){
-            // Removing any digits in front, then removing all the digit behind the number
-            if(i+1 < digits){currentDigit = Math.floor((origNum % Math.pow(10, digits-(i))) / Math.pow(10, (digits-(i+1))));}
-            // Just removing digits in front if it is a single digit to avoid nullDivision
-            else{currentDigit = origNum % Math.pow(10, digits-(i));}
+            int onedCurrPlace = (int)Math.pow(10, digits-(i));
+            int onedLeftPlace = (int)Math.pow(10, (digits-(i+1)));
+            
+            
+            // Removing any digits in front, then removing all the digits behind the current place we are looking.
+            currentDigit = (int)Math.floor((origNum % onedCurrPlace) / onedLeftPlace);
 
             // First check if the digit is a 4 or a 9, if else then we can do modulo 5 then the remaining smaller value
             // Removing any digits in front, then removing all the digit behind the number
-            if(currentDigit != 4.0 && currentDigit != 9.0){
-                if(currentDigit >= 5.0){
-                    romanNum += Character.toString(myMap.get(5 * Math.pow(10, digits-(i+1))));
-                    currentDigit -= 5.0;
+            if(currentDigit != 4 && currentDigit != 9){
+                
+                // If the number is between 4 and 9 (exclusive for both endpoints), then we need to first add the 5-roman
+                // numeral.
+                if(currentDigit >= 5){
+                    
+                    // Getting the correct 5-roman numeral
+                    romanNum.append(myMap.get(5 * onedLeftPlace));
+                    currentDigit -= 5;
                 }
-                while(currentDigit > 0){
-                    romanNum += Character.toString(myMap.get(Math.pow(10, digits-(i+1))));
-                    --currentDigit;
+                
+                // Remaining amount after 5 roman-character has been placed.
+                for(int j = 0; j < currentDigit; ++j){
+                    romanNum.append(myMap.get(onedLeftPlace));
                 }
             }
+            
+            // If digit is a four or a nine
             else{
-                if(currentDigit == 9.0){
-                    romanNum += Character.toString(myMap.get(Math.pow(10, digits-(i+1))));
-                    romanNum += Character.toString(myMap.get(Math.pow(10, digits-(i))));
+                if(currentDigit == 9){
+                    
+                    // 9 is special because we need to get the 1'd version of the current position and
+                    // then get the 10'd version of the current position (which is just using i instead of i+1)
+                    romanNum.append(myMap.get(onedLeftPlace));
+                    romanNum.append(myMap.get(onedCurrPlace));
                 }
                 else{
-                    romanNum += Character.toString(myMap.get(Math.pow(10, digits-(i+1))));
-                    romanNum += Character.toString(myMap.get(5 * Math.pow(10, digits-(i+1))));
+                    
+                    // 4 is special because we need to get the 1'd version of the current position and
+                    // then get the 5'd version of the current position
+                    romanNum.append(myMap.get(onedLeftPlace));
+                    romanNum.append(myMap.get(5 * onedLeftPlace));
                 }
             }
         }
-        return romanNum;
-    }
-
-    public static void main(String[] args){
-        int myNum = 1994;
-        String myRomanNum = intToRoman(myNum);
-        System.out.println(myRomanNum);
-        return;
+        return romanNum.toString();
     }
 }
--- SlidingWindows/trappingRainWater.java
+++ SlidingWindows/trappingRainWater.java
@@ -1,71 +1,42 @@
+// Gist: We use two pointers one from the left and one from the right and continuously check each step if the value at the
+// other pointer is smaller. If so, the smaller pointer will always be deciding factor that will determine how much
+// water to add to the total. We could use only one pointer if every dip in the pillars returned to the max value found,
+// but this is not always the case. This is why we need to know which side has the currently smaller pillar.
+// TC: O(n), SC: O(1)
 class Solution {
-    public int recurseBoundaries(int[] height, int maxHeight, int maxHeightIndex, int leftBoundary, int leftBoundaryIndex, int rightBoundary, int rightBoundaryIndex){
-        // Base Case where we return 0 if the boundaries are both invalid
-        if(leftBoundary == -1 && rightBoundary == -1){return 0;}
-        boolean leftFound = false, rightFound = false;
-        int rightSum = 0, leftSum = 0;
-        if(leftBoundary != -1){
-            // Getting the leftBoundary if it exists
-            for(int i = maxHeightIndex - 1; i > -1; --i){
-                if(height[i] > leftBoundary){
-                    leftBoundaryIndex = i;
-                    leftBoundary = height[i];
-                    leftFound = true;
-                }
-                // Stopping early in the case that we find a pillar of the same height
-                if(height[i] >= maxHeight){break;}
-            }
-            // Adding all the values up and recursing if a rain water was trapped
-            if(leftFound){
-                // Adding all the rain water to the left of maxHeightIndex
-                for(int i = leftBoundaryIndex; i < maxHeightIndex; ++i){
-                    leftSum += (leftBoundary - height[i]);
-                }
-                leftSum += recurseBoundaries(height, leftBoundary, leftBoundaryIndex, 0, 0, -1, 0);
-            }
-        }
-        if(rightBoundary != -1){
-            // Getting the rightBoundary if it exists
-            for(int i = maxHeightIndex + 1; i < height.length; ++i){
-                if(height[i] > rightBoundary){
-                    rightBoundaryIndex = i;
-                    rightBoundary = height[i];
-                    rightFound = true;
-                }
-                // Stopping early in the case that we find a pillar of the same height
-                if(height[i] >= maxHeight){break;}
-            }
-            if(rightFound != true){
-                rightBoundary = -1;
-            }
-            else{
-                // Adding all the rain water to the left of maxHeighIndex
-                for(int i = rightBoundaryIndex; i > maxHeightIndex; --i){
-                    rightSum += (rightBoundary - height[i]);
-                }
-                rightSum += recurseBoundaries(height, rightBoundary, rightBoundaryIndex, -1, 0, 0, 0);
-            }
-        }
-        return rightSum + leftSum;
-    }
-        
     public int trap(int[] height) {
-        if(height.length == 1 || height.length == 0){return 0;}
-        int maxHeight = 0, maxHeightIndex = 0;
-        // Finding max height
-        for(int i = 0; i < height.length; ++i){
-            if(height[i] > maxHeight){maxHeight=height[i];maxHeightIndex = i;}
+        int maxRight = height[height.length-1], maxLeft = height[0], leftHead = 0, rightHead = height.length-1;
+        int rainWater = 0;
+        while(leftHead < rightHead){
+            
+            // If the current value at the left head is smaller than the right head's value
+            if(height[leftHead] < height[rightHead]){
+                
+                // If we find a new maxLeft
+                if(height[leftHead] >= maxLeft){
+                    maxLeft = height[leftHead] ;
+                }
+                
+                // If the current leftHead value is not larger than the maxLeft
+                else{rainWater += (maxLeft - height[leftHead]);}
+                leftHead++;
+            }
+            
+            // If the righthead is larger in value
+            else{
+                
+                // If we find a value with the right head that is greater than the current maxRight
+                if(height[rightHead] >= maxRight){
+                    maxRight = height[rightHead];
+                }
+                
+                // If the current rightHead is less than the maxRight we get the rain water
+                else{
+                    rainWater += maxRight - height[rightHead];
+                }
+                rightHead--;
+            }
         }
-        //System.out.println("Here is the initial max height:" + maxHeight);
-        return recurseBoundaries(height, maxHeight, maxHeightIndex, 0, 0, 0, 0);
+        return rainWater;
     }
-
-    //public static void main(String[] args){
-    //    Solution mySolution = new Solution();
-    //    //int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
-    //    //int[] height = {4, 2, 0, 3, 2, 5};
-    //    //int[] height = {2, 1, 2, 1, 2, 1, 2, 1, 2, 1};
-    //    int myAnswer = mySolution.trap(height);
-    //    System.out.println(myAnswer);
-    //}
 }
